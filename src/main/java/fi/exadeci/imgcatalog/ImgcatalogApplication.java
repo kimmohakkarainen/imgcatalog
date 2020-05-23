49444ed34bab35926fb12e9942d589b2b120ab8a
kimmohakkarainen <30873934+kimmohakkarainen@users.noreply.github.com>
2020-05-23 20:34:00 +0300

    initial files

---
+++ src/main/java/fi/exadeci/imgcatalog/ImgcatalogApplication.java
@@ -0,0 +1,13 @@
+package fi.exadeci.imgcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgcatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/exadeci/imgcatalog/ImgcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.exadeci.imgcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
